c4e4a3691b3676729c685eab320a603d8f091e65
Matoto2 <martinc@lithiummarketing.com>
2022-12-07 11:20:49 -0500

    try target static

--- nuxt.config.js
+++ nuxt.config.js
@@ -82,6 +82,7 @@ export default {
 		locales: ["fr"]
 	},
 
+	target: 'static',
 	// Modules: https://go.nuxtjs.dev/config-modules
 	modules: [
 		// Doc: https://www.primefaces.org/primevue/showcase-v2/#/setup
