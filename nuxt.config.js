3c9ddf641d1e235f7fceecefcd061752b6a65eb3
Matoto2 <martinc@lithiummarketing.com>
2022-12-07 16:29:50 -0500

    api https

--- nuxt.config.js
+++ nuxt.config.js
@@ -105,7 +105,7 @@ export default {
 	// Axios module configuration: https://go.nuxtjs.dev/config-axios
 	axios: {
 		// Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-		baseURL: 'http://api.granby-profitez.com/v1',
+		baseURL: 'https://api.granby-profitez.com/v1',
 	},
 
 	// Build Configuration: https://go.nuxtjs.dev/config-build
